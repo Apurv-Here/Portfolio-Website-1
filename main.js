d5045631ab7cd2af504a101319dbc3ebb7069f38
APURV CHOUBEY <apurv.main2@gmail.com>
2022-08-01 02:46:28 +0530

    updated project section

--- main.js
+++ main.js
@@ -2,16 +2,12 @@
 let count = 0;
 let hamFlag = false;
 const hamBurgerShow = mobileMenuShow = () =>{
-    console.log('click 1');
     let element = document.getElementById('hamBurgerMenu');
-    // console.log(element);
-    // element.classList.add('visible');
-    // element.classList.add('block');
+    
 
     if(count == 1){
         element.classList.toggle('hidden');
         element.classList.toggle('invisible');
-        
     }
 
     if(count<1){
@@ -19,7 +15,6 @@ const hamBurgerShow = mobileMenuShow = () =>{
         element.classList.remove('hidden');
         element.classList.remove('invisible');
     }
-    console.log('click 2');
 
 }
 // code for hamburger menu for mobile ends
